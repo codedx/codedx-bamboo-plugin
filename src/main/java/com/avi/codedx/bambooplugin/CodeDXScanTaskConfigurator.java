11a4c3acc4786fcc1049f54e676e88bbfc2aa149
John Schroeder <John.Schroeder@avi.com>
2018-12-07 20:45:28 -0500

    Populate list of projects from CodeDx

--- src/main/java/com/avi/codedx/bambooplugin/CodeDXScanTaskConfigurator.java
+++ src/main/java/com/avi/codedx/bambooplugin/CodeDXScanTaskConfigurator.java
@@ -4,8 +4,14 @@ import com.atlassian.bamboo.collections.ActionParametersMap;
 import com.atlassian.bamboo.task.AbstractTaskConfigurator;
 import com.atlassian.bamboo.task.TaskDefinition;
 import com.atlassian.bamboo.utils.error.ErrorCollection;
+import com.avi.codedx.client.ApiClient;
+import com.avi.codedx.client.ApiException;
+import com.avi.codedx.client.api.Project;
+import com.avi.codedx.client.api.ProjectsApi;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class CodeDXScanTaskConfigurator extends AbstractTaskConfigurator {
@@ -43,6 +49,36 @@ public class CodeDXScanTaskConfigurator extends AbstractTaskConfigurator {
         }
     }
 
+    private static List<Project> getProjectList() {
+
+        ServerConfigManager serverConfigManager = ServerConfigManager.getInstance();
+
+        // TODO: Put this back in and remove hardcoded values after getting persistence working
+        /*
+        final String apiKey = serverConfigManager.getApikey();
+        final String apiUrl = serverConfigManager.getUrl();
+        */
+
+        final String apiKey = "f9a03424-d304-46a9-8ed6-03886ca20b85";
+        final String apiUrl = "http://localhost:8080";
+
+        ApiClient cdxApiClient = new ApiClient();
+        cdxApiClient.setBasePath(apiUrl);
+        cdxApiClient.setApiKey(apiKey);
+
+        ProjectsApi projectsApi = new ProjectsApi();
+        projectsApi.setApiClient(cdxApiClient);
+
+        // TODO: Fix hang on connection refused
+        try {
+            return projectsApi.getProjects().getProjects();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<Project>();
+    }
+
     @Override
     public void populateContextForCreate(final Map<String, Object> context)
     {
@@ -51,6 +87,7 @@ public class CodeDXScanTaskConfigurator extends AbstractTaskConfigurator {
         context.put("url", "http://localhost:8080");
         context.put("apikey", "api-key-goes-here");
         context.put("analysisname", "Bamboo Analysis");
+        context.put("projectList", getProjectList());
     }
 
     @Override
@@ -61,5 +98,8 @@ public class CodeDXScanTaskConfigurator extends AbstractTaskConfigurator {
         context.put("url", taskDefinition.getConfiguration().get("url"));
         context.put("apikey", taskDefinition.getConfiguration().get("apikey"));
         context.put("analysisname", taskDefinition.getConfiguration().get("analysisname"));
+
+        // TODO: Save and load plan config
+        //context.put("projectlist",taskDefinition.getConfiguration().get("projectlist"));
     }
 }
--- src/main/resources/configureCodeDxTask.ftl
+++ src/main/resources/configureCodeDxTask.ftl
@@ -1,5 +1,18 @@
-[@ww.select labelKey="Code DX Project" name="codeDxProjectName" list="codeDxProjectList" required="true" toggle="true"/]
-[@ww.textfield label="Code DX Analysis Name" name="analysisname" required='true'/]
-[#--@ww.selectlabel="List of Projects" id="projectlist" name="projectlist" list="interfaceprojectlist" listKey="projectId" listValue="projectName" value="selectedProjectList" size="3" required="true"/]
+[@ww.select
+	label="Code DX Project"
+	id="project"
+    name="project"
+    list="projectList"
+    listKey="id"
+    listValue="name"
+	required="true"
+	toggle="true"
+/]
 
 [@ww.submit value="Update Projects" name="submitname" theme='simple'/]
+
+[@ww.textfield
+	label="Code DX Analysis Name"
+	name="analysisname"
+	required='true'
+/]

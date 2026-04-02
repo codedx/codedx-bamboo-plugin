8b05f286a164488e5658a1c788fb622922606590
Tyler Camp <tylerc@blackduck.com>
2026-04-02 10:12:28 -0400

    bump dependencies, fix compile errors

--- src/main/java/com/codedx/client/CustomInstantDeserializer.java
+++ src/main/java/com/codedx/client/CustomInstantDeserializer.java
@@ -8,8 +8,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.datatype.threetenbp.DecimalUtils;
 import com.fasterxml.jackson.datatype.threetenbp.deser.ThreeTenDateTimeDeserializerBase;
-import com.fasterxml.jackson.datatype.threetenbp.function.BiFunction;
-import com.fasterxml.jackson.datatype.threetenbp.function.Function;
 import org.threeten.bp.*;
 import org.threeten.bp.format.DateTimeFormatter;
 import org.threeten.bp.temporal.Temporal;
@@ -17,6 +15,8 @@ import org.threeten.bp.temporal.TemporalAccessor;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * Deserializer for ThreeTen temporal {@link Instant}s, {@link OffsetDateTime}, and {@link ZonedDateTime}s.
@@ -208,7 +208,7 @@ public class CustomInstantDeserializer<T extends Temporal>
         return value;
       }
     }
-    throw context.mappingException("Expected type float, integer, or string.");
+    throw context.instantiationException(_valueClass, "Expected type float, integer, or string.");
   }
 
   private ZoneId getZone(DeserializationContext context) {
